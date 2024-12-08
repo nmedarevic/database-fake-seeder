7b37e3a296ab23c6da13895ffc15d0534c8066b3
Nikola Medarevic <nikola@petmediagroup.com>
2024-12-08 23:35:25 +0100

    Writes data into db

--- go.mod
+++ go.mod
@@ -7,3 +7,12 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,77 +1,66 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/brianvoe/gofakeit/v7"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-		err := godotenv.Load()
+	err := godotenv.Load()
 
-		if err != nil {
-			log.Fatal("Error loading .env file")
+	if err != nil {
+		log.Fatal("Error loading .env file")
 
-			return
-		}
+		return
+	}
 
-		// Retrieve environment variable with existence check
-		dbUrl, exists := os.LookupEnv("DB_URL")
-		if !exists {
-				log.Fatal("DB_URL is not set")
-		}
+	// Retrieve environment variable with existence check
+	dbUrl, exists := os.LookupEnv("DB_URL")
+	if !exists {
+		log.Fatal("DB_URL is not set")
+	}
 
-		// Open a connection to the database
-		db, err := sql.Open("postgres", dbUrl)
-		if err != nil {
-				log.Fatal(err)
-		}
-		defer db.Close()
- 
-		 // Test the connection
-		 err = db.Ping()
-		 if err != nil {
-				 log.Fatal(err)
-		 }
- 
-		 fmt.Println("Successfully connected to PostgreSQL database!")
- 
-		 // Example of a simple query
-		 rows, err := db.Query("SELECT id, username FROM \"user\" LIMIT 5")
-		 if err != nil {
-				 log.Fatal(err)
-		 }
-		 defer rows.Close()
- 
-		 // Iterate through results
-		 for rows.Next() {
-				 var id int
-				 var name string
-				 if err := rows.Scan(&id, &name); err != nil {
-						 log.Fatal(err)
-				 }
-				 fmt.Printf("ID: %d, Name: %s\n", id, name)
-		 }
- 
-		 // Check for any errors encountered during iteration
-		 if err = rows.Err(); err != nil {
-				 log.Fatal(err)
-		 }
+	// Open a connection to the database
+	db, err := pgx.Connect(context.Background(), dbUrl)
 
-	fmt.Println(gofakeit.Name())
-	fmt.Println(gofakeit.LastName())
-	fmt.Println(gofakeit.FirstName())
-	fmt.Println(gofakeit.PastDate().UTC())
-	fmt.Println(gofakeit.FutureDate())
-	fmt.Println(gofakeit.BuzzWord())
-	fmt.Println(gofakeit.Company())
-	fmt.Println(gofakeit.HackerPhrase())
-	fmt.Println(gofakeit.HackerNoun())
-	fmt.Println(gofakeit.BS())
-	fmt.Println(gofakeit.BeerStyle())
+	// db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close(context.Background())
+
+	// Test the connection
+	err = db.Ping(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to PostgreSQL database!")
+
+	rowsData := [][]interface {
+	}{
+		{uuid.New().String(), gofakeit.Email(), gofakeit.Password(false, false, false, false, false, 0), gofakeit.PastDate(), gofakeit.PastDate(), gofakeit.Username(), gofakeit.Name()},
+		{uuid.New().String(), gofakeit.Email(), gofakeit.Password(false, false, false, false, false, 0), gofakeit.PastDate(), gofakeit.PastDate(), gofakeit.Username(), gofakeit.Name()},
+	}
+
+	numberOfRowsInserted, err := db.CopyFrom(
+		context.Background(),
+		pgx.Identifier{"user"},
+		[]string{"id", "email", "password", "created_at", "updated_at", "username", "full_name"},
+		pgx.CopyFromRows(rowsData),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Rows inserted %d\n", numberOfRowsInserted)
 }
